da5ee7b58e6b1186c4f7f0b9dc2727be4a43d98b
Colin Woon <colin.wyj@gmail.com>
2024-12-17 17:53:51 +0800

    feat: basic tokenization without handling edgecase
    - able to split strings
    - able to duplicate strings into token struct array

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: cwoon <cwoon@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 17:29:55 by cwoon             #+#    #+#             */
-/*   Updated: 2024/12/17 15:21:43 by cwoon            ###   ########.fr       */
+/*   Updated: 2024/12/17 17:21:52 by cwoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,30 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+# define TO_SPLIT -69
+
+typedef enum e_token_type {
+	PIPE,
+	COMMAND,
+	REDIRECTION,
+	COMMAND_FLAG,
+	STRING,
+}	t_token_type;
+
 typedef struct s_token {
-	char *value;
-	char *type;
-}	d_token;
+	char			*value;
+	t_token_type	*type;
+	// t_token	*prev;
+	// t_token	*next;
+}	t_token;
 
+typedef struct s_command {
 
+}	t_command;
+
+typedef struct s_data {
+	char	**strings;
+	char	*readline;
+	int		total_tokens;
+	t_token	*tokens;
+}	t_data;
--- srcs/main.c
+++ srcs/main.c
@@ -6,21 +6,82 @@
 /*   By: cwoon <cwoon@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 17:30:18 by cwoon             #+#    #+#             */
-/*   Updated: 2024/12/17 15:23:23 by cwoon            ###   ########.fr       */
+/*   Updated: 2024/12/17 17:52:06 by cwoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	tokenization(t_data *data);
+void	get_strings(char ***strings, char *readline);
+void	initialize_tokens(t_data *data);
+
 int main()
 {
-	char	*line;
+	t_data data;
 
 	while (1)
 	{
-		line = readline("minishell > ");
-		add_history(line);
-		printf("%s\n", line);
-		free(line);
+		data.readline = readline("minishell > ");
+		add_history(data.readline);
+		tokenization(&data);
+		// printf("%s\n", readline);
+		free(data.readline);
+	}
+}
+
+void	tokenization(t_data *data)
+{
+	int	count;
+
+	get_strings(&data->strings, data->readline);
+	count = 0;
+	while (data->strings[count] != NULL)
+		count++;
+	data->total_tokens = count;
+	initialize_tokens(data);
+	printf("%d\n", data->total_tokens);
+}
+
+void	get_strings(char ***strings, char *readline)
+{
+	int		i;
+	int		in_quote;
+
+	i = 0;
+	in_quote = 0;
+	while (readline[i])
+	{
+		if (readline[i] == SPACE && !in_quote)
+			readline[i] = TO_SPLIT;
+		else if ((readline[i] == '\'' || readline[i] == '"') && !in_quote)
+			in_quote = 1;
+		else if ((readline[i] == '\'' || readline[i] == '"') && in_quote)
+			in_quote = 0;
+		i++;
+	}
+	*strings = ft_split(readline, TO_SPLIT);
+
+	i = 0;
+	while ((*strings)[i] != NULL)
+	{
+		printf("Token %d: %s\n", i, (*strings)[i]);
+		i++;
+	}
+}
+
+void initialize_tokens(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	data->tokens = malloc(data->total_tokens * sizeof(t_token));
+	// if (data.tokens = NULL)
+	// 	return(handle_error(data));
+	while (data->strings[i] != NULL)
+	{
+		data->tokens[i].value = ft_strdup((const char *)data->strings[i]);
+		printf("Token %d: %s\n", i, data->tokens[i].value);
+		i++;
 	}
 }
