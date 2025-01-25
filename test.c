2a09fa67b5ed0c46e209ac3af84b1fcc6b58f767
Joe Ow <joeow@Joes-MacBook-Air.local>
2025-01-25 15:01:07 +0800

    chore: added test.c

--- test.c
+++ test.c
@@ -1,12 +1,27 @@
 #include "minishell.h"
 
+int	current_directory(t_data *data)
+{
+	char	*cwd;
+
+	cwd = getcwd(NULL, 0);
+	if (!cwd)
+	{
+		g_last_exit_code = 1;
+		return (1);
+	}
+	ft_putendl_fd(cwd, 1);
+	free(cwd);
+	return (0);
+}
+
 int main(int ac, char **av)
 {
 	(void) ac;
 	char *input = av[1];
 
-	if (!ft_strncmp(input, "echo", 4))
+	if (!ft_strncmp(input, "pwd", 4))
 	{
-		printf("this is %s\n",input);
+		current_directory(NULL);
 	}
 }
