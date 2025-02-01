28ddab8fe4f9a3e8f749d7fd5381649cd11a39f9
Colin Woon <colin.wyj@gmail.com>
2025-02-01 17:06:30 +0800

    fix: set_env_var

--- srcs/execution/builtins/env_utils.c
+++ srcs/execution/builtins/env_utils.c
@@ -6,7 +6,7 @@
 /*   By: cwoon <cwoon@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 23:53:36 by jow               #+#    #+#             */
-/*   Updated: 2025/02/01 16:38:26 by cwoon            ###   ########.fr       */
+/*   Updated: 2025/02/01 17:05:44 by cwoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,32 +31,13 @@ int	get_env_var_index(t_data *data, char *key)
 	return (EXIT_FAILURE);
 }
 
-// int	set_env_var(t_data *data, char *key, char *tmp)
-// {
-// 	int		i;
+int	set_env_var(t_data *data, char *key, char *tmp)
+{
+	int		i;
 
-// 	i = get_env_var_index(data, key);
-// 	data->envp_array[i] = replace_substring(data->envp_array[i], getenv(key), tmp);
-// 	if (!data->envp_array[i])
-// 		return (EXIT_FAILURE);
-// 	if (i != 1 && data->envp_array[i])
-// 	{
-// 		free(data->envp_array[i]);
-// 		data->envp_array[i] = ft_strjoin(key, new_env);
-// 	}
-// 	else
-// 	{
-// 		i = 0;
-// 		while (data->envp_array[i])
-// 			i++;
-// 		data->envp_array = realloc(data->envp_array, i + 1);
-// 		if (!data->envp_array)
-// 		{
-// 			free(new_env);
-// 			return (EXIT_FAILURE);
-// 		}
-// 		data->envp_array[i] = ft_strjoin(key, new_env);
-// 	}
-// 	free(new_env);
-// 	return (EXIT_SUCCESS);
-// }
+	i = get_env_var_index(data, key);
+	data->envp_array[i] = replace_substring(data->envp_array[i], getenv(key), tmp);
+	if (!data->envp_array[i])
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
+}
