2ead2cc7e87dffe12516ac258bf786ae1f854ba7
Joe Ow <joeow@Joes-MacBook-Air.local>
2025-02-01 15:29:03 +0800

    chore: changed function name

--- srcs/execution/builtins/pwd.c
+++ srcs/execution/builtins/pwd.c
@@ -6,18 +6,19 @@
 /*   By: jow <jow@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 14:20:39 by jow               #+#    #+#             */
-/*   Updated: 2025/01/27 20:28:03 by jow              ###   ########.fr       */
+/*   Updated: 2025/01/28 14:57:58 by jow              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_cd(t_data *data);
+int	ft_pwd(t_data *data, char **args);
 
-int	ft_cd(t_data *data)
+int	ft_pwd(t_data *data, char **args)
 {
 	char	*cwd;
 
+	(void)args;
 	cwd = getcwd(NULL, 0);
 	if (!cwd)
 		return (1);
