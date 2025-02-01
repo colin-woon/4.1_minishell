4311f196a4d35b7e7e34282027a8807e50e718be
Joe Ow <joeow@Joes-MacBook-Air.local>
2025-02-02 02:10:09 +0800

    fix: convert_envp returns wrong value

--- srcs/execution/execute_utils.c
+++ srcs/execution/execute_utils.c
@@ -6,35 +6,44 @@
 /*   By: jow <jow@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 01:28:06 by jow               #+#    #+#             */
-/*   Updated: 2025/02/02 01:41:04 by jow              ###   ########.fr       */
+/*   Updated: 2025/02/02 02:07:46 by jow              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	**convert_envp(t_envp *envp)
-{
-	int		i;
-	t_envp	*temp;
+char	**convert_envp(t_envp *envp) {
+	int i;
+	t_envp *temp;
+	char **envp_array;
+	char *env_var;
 
 	i = 0;
 	temp = envp;
-	while (temp)
-	{
+	while (temp) {
 		i++;
 		temp = temp->next;
 	}
-	char **envp_array = (char **)malloc(sizeof(char *) * (i + 1));
+	envp_array = (char **)malloc(sizeof(char *) * (i + 1));
 	if (!envp_array)
-		return (NULL);
+		return NULL;
 	temp = envp;
 	i = 0;
 	while (temp)
 	{
-		envp_array[i] = ft_strdup(temp->variable_name);
+		env_var = ft_strjoin(temp->variable_name, "=");
+		envp_array[i] = ft_strjoin(env_var, temp->value);
+		free(env_var);
+		if (!envp_array[i])
+		{
+			while (i > 0)
+				free(envp_array[--i]);
+			free(envp_array);
+			return NULL;
+		}
 		temp = temp->next;
 		i++;
 	}
 	envp_array[i] = NULL;
-	return (envp_array);
+	return envp_array;
 }
