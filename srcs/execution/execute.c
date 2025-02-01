1be39a7074541b904394db1d95e993be166eb400
Joe Ow <joeow@Joes-MacBook-Air.local>
2025-02-01 15:31:59 +0800

    chore: uncommented builtins

--- srcs/execution/execute.c
+++ srcs/execution/execute.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execute.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cwoon <cwoon@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*   By: jow <jow@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 16:52:21 by cwoon             #+#    #+#             */
-/*   Updated: 2025/01/27 16:57:13 by cwoon            ###   ########.fr       */
+/*   Updated: 2025/01/28 15:38:42 by jow              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,27 +30,30 @@ void	execute_commands(t_data *data)
 		restore_stdio(data->cmd->io_fds);
 	}
 	if (is_exit)
+	{
+		printf("is_exit: %d\n", is_exit);
 		return ;
+	}
 	// return (create_children(data));
 }
 int	execute_builtin(t_data *data, t_cmd *cmd)
 {
-	int	is_exit;
+	int	is_not_exit;
+	is_not_exit = 1;
 
-	is_exit = 1;
-	// if (ft_strncmp(cmd->name, "cd", 3) == 0)
-	// 	is_exit = cd_builtin(data, cmd->args);
+	if (ft_strncmp(cmd->name, "cd", 3) == 0)
+		is_not_exit = ft_cd(data, cmd->args);
 	// else if (ft_strncmp(cmd->name, "echo", 5) == 0)
-	// 	is_exit = echo_builtin(data, cmd->args);
-	// else if (ft_strncmp(cmd->name, "env", 4) == 0)
-	// 	is_exit = env_builtin(data, cmd->args);
-	// else if (ft_strncmp(cmd->name, "export", 7) == 0)
-	// 	is_exit = export_builtin(data, cmd->args);
-	// else if (ft_strncmp(cmd->name, "pwd", 4) == 0)
-	// 	is_exit = pwd_builtin(data, cmd->args);
+	// 	is_not_exit = echo_builtin(data, cmd->args);
+	else if (ft_strncmp(cmd->name, "env", 4) == 0)
+		is_not_exit = ft_env(data, cmd->args);
+	else if (ft_strncmp(cmd->name, "export", 7) == 0)
+		is_not_exit = ft_export(data, cmd->args);
+	else if (ft_strncmp(cmd->name, "pwd", 4) == 0)
+		is_not_exit = ft_pwd(data, cmd->args);
 	// else if (ft_strncmp(cmd->name, "unset", 6) == 0)
-	// 	is_exit = unset_builtin(data, cmd->args);
+	// 	is_not_exit = unset_builtin(data, cmd->args);
 	// else if (ft_strncmp(cmd->name, "exit", 5) == 0)
-	// 	is_exit = exit_builtin(data, cmd->args);
-	return (is_exit);
+	// 	is_not_exit = exit_builtin(data, cmd->args);
+	return (is_not_exit);
 }
