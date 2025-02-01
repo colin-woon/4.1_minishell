a41b150b62f6e5023436a20b2d4f5c007c3ba130
Joe Ow <joeow@Joes-MacBook-Air.local>
2025-02-02 02:31:01 +0800

    feat: added echo builtin

---
+++ srcs/execution/builtins/echo.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jow <jow@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/02 02:11:12 by jow               #+#    #+#             */
+/*   Updated: 2025/02/02 02:22:56 by jow              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	ft_echo(t_data *data, char **args)
+{
+	int	i;
+	int	n_flag;
+
+	(void)data;
+	i = 1;
+	n_flag = 0;
+	while (args[i] && ft_strncmp(args[i], "-n", 2) == 0)
+	{
+		n_flag = 1;
+		i++;
+	}
+	while (args[i])
+	{
+		ft_putstr_fd(args[i], 1);
+		if (args[i + 1])
+			ft_putstr_fd(" ", 1);
+		i++;
+	}
+	if (!n_flag)
+		ft_putstr_fd("\n", 1);
+	return (0);
+}
