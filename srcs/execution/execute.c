abb4a76ca92d82abe7e480df85588fedd2705566
Joe Ow <joeow@Joes-MacBook-Air.local>
2025-02-02 15:14:47 +0800

    fix: -removed convert_envp part at execute_binary      -added convert_envp at line 46

--- srcs/execution/execute.c
+++ srcs/execution/execute.c
@@ -6,7 +6,7 @@
 /*   By: jow <jow@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 16:52:21 by cwoon             #+#    #+#             */
-/*   Updated: 2025/02/02 02:16:16 by jow              ###   ########.fr       */
+/*   Updated: 2025/02/02 15:12:07 by jow              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ int	execute_builtin(t_data *data, t_cmd *cmd)
 	int	is_not_exit;
 
 	is_not_exit = CMD_NOT_FOUND;
+	data->envp_array = convert_envp(data->our_envp);
 	if (ft_strncmp(cmd->name, "cd", ft_strlen("cd")) == 0)
 		is_not_exit = ft_cd(data, cmd->args);
 	else if (ft_strncmp(cmd->name, "echo", 5) == 0)
@@ -119,18 +120,10 @@ int	execute_binary(t_data *data, t_cmd *cmd)
 	exit_status = is_invalid_command(cmd);
 	if (exit_status == CMD_NOT_FOUND || exit_status == CMD_NOT_EXECUTABLE)
 		return (exit_status);
-	data->envp_array = convert_envp(data->our_envp);
-	if (!data->envp_array)
-	{
-		print_errno_str("malloc", strerror(errno));
-		return (errno);
-	}
 	if (execve(cmd->args[0], cmd->args, data->envp_array) == -1)
 	{
 		print_errno_str("execve", strerror(errno));
-		ft_free_2d_array(data->envp_array);
 		return (FAILURE);
 	}
-	ft_free_2d_array(data->envp_array);
 	return (FAILURE);
 }
