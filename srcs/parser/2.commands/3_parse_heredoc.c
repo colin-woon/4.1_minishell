1494ad788c1e6b40a751ed09979720cec4783d73
Colin Woon <colin.wyj@gmail.com>
2025-02-05 20:45:31 +0800

    refactor: using cleanup function better readability

--- srcs/parser/2.commands/3_parse_heredoc.c
+++ srcs/parser/2.commands/3_parse_heredoc.c
@@ -6,15 +6,17 @@
 /*   By: cwoon <cwoon@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 17:05:49 by cwoon             #+#    #+#             */
-/*   Updated: 2025/02/05 20:37:42 by cwoon            ###   ########.fr       */
+/*   Updated: 2025/02/05 20:44:55 by cwoon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	parse_heredoc(t_cmd **last_cmd, t_token **tokens);
-void	run_heredoc(t_io_fds *io);
-int		is_matching_heredoc_limiter(char *input, char *limiter);
+void		parse_heredoc(t_cmd **last_cmd, t_token **tokens);
+void		run_heredoc(t_io_fds *io);
+int			is_matching_heredoc_limiter(char *input, char *limiter);
+static void	clean_up(char **input, char **temp);
+
 
 void	parse_heredoc(t_cmd **last_cmd, t_token **tokens)
 {
@@ -26,7 +28,8 @@ void	parse_heredoc(t_cmd **last_cmd, t_token **tokens)
 	init_io_fds(cmd);
 	cmd->io_fds->infile = HEREDOC_FILE;
 	cmd->io_fds->heredoc_limiter = ft_strdup(temp->next->value);
-	cmd->io_fds->fd_in = open(cmd->io_fds->infile, O_WRONLY | O_APPEND | O_CREAT, 0644);
+	cmd->io_fds->fd_in = open(cmd->io_fds->infile, \
+	O_WRONLY | O_APPEND | O_CREAT, 0644);
 	run_heredoc(cmd->io_fds);
 	close(cmd->io_fds->fd_in);
 	cmd->io_fds->fd_in = open(cmd->io_fds->infile, O_RDONLY);
@@ -51,15 +54,14 @@ void	run_heredoc(t_io_fds *io)
 		if (input == NULL)
 			return (print_errno_str("warning", io->heredoc_limiter, \
 			"here-document delimited by end-of-file"), \
-			free_ptr((void **)&input), free_ptr((void **)&temp));
+			clean_up(&input, &temp));
 		if (is_matching_heredoc_limiter(input, io->heredoc_limiter))
 			break ;
 		temp = ft_strjoin(input, "\n");
 		write(io->fd_in, temp, ft_strlen(temp));
-		free_ptr((void **)&input);
-		free_ptr((void **)&temp);
+		clean_up(&input, &temp);
 	}
-	return (free_ptr((void **)&input), free_ptr((void **)&temp));
+	return (clean_up(&input, &temp));
 }
 
 int	is_matching_heredoc_limiter(char *input, char *limiter)
@@ -70,9 +72,8 @@ int	is_matching_heredoc_limiter(char *input, char *limiter)
 	return (0);
 }
 
-// static void	clean_up(char *input, char *temp)
-// static void	clean_up(char *input, char *temp)
-// {
-// 		free_ptr((void **)&input);
-// 		free_ptr((void **)&temp);
-// }
+static void	clean_up(char **input, char **temp)
+{
+		free_ptr((void **)input);
+		free_ptr((void **)temp);
+}
