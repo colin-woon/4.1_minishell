f2a5292565805219e6382c7a9708837f67156862
joeow06 <joeow06@gmail.com>
2025-02-11 00:49:59 +0800

    fix: pwd bug, error msgs are more bashlike now

--- srcs/execution/builtins/pwd.c
+++ srcs/execution/builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: jow <jow@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 14:20:39 by jow               #+#    #+#             */
-/*   Updated: 2025/02/06 17:11:24 by jow              ###   ########.fr       */
+/*   Updated: 2025/02/11 00:47:05 by jow              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,14 @@ int	ft_pwd(char **args);
 int	ft_pwd(char **args)
 {
 	char	*cwd;
+	char	option[3];
 
-	if (args[1])
+	if (args[1] && args[1][0] == '-')
 	{
-		print_errno_str("pwd", args[1], "invalid option");
+		option[0] = '-';
+		option[1] = args[1][1];
+		option[2] = '\0';
+		print_errno_str("pwd", option, "invalid option");
 		return (1);
 	}
 	cwd = getcwd(NULL, 0);
